2c5cebfe2e55fb5b907a427dece3ba17e608230c
Cameron Wheeler <cameron@scelos.com>
2016-01-23 00:34:57 -0500

    Wired up socket.io to Express middleware

--- middleware/statly.js
+++ middleware/statly.js
@@ -1,9 +1,39 @@
 var _appName;
 var _app;
 var _staticDir;
+var _client;
+var _key;
 var _routes = [];
 
- var fs = require("fs");
+var socket;
+
+var fs = require("fs");
+var os = require("os");
+var io = require("socket.io-client");
+
+function sendData(tag, props) {
+	if (socket == undefined) {
+		throw "Socket undefined, connection likely failed. Check network connection."
+	} else {
+		if (!socket.connected) {
+			socket = io.connect('localhost', {
+			    port: 3000
+			});
+		}
+
+		props.client = _client;
+		props.key = _key;
+		props.appName = _appName;
+
+		socket.emit(tag, props);
+	}
+}
+
+function sendLoadAvg() {
+	sendData('loadAvg', {load: os.loadavg()});
+
+	setTimeout(sendLoadAvg, 1000*60*5);
+}
 
 module.exports = {
 	levels: {
@@ -13,13 +43,13 @@ module.exports = {
 		critical : 3
 	},
 	log: function(level, message) {
-
+		
 	},
 	increaseCounter: function(counter) {
-
+		sendData('increaseCounter', {counter: counter});
 	},
 	decreseCounter: function(counter) {
-
+		sendData('decreseCounter', {counter: counter});
 	},
 	clockRequest: function(req, res) {
 		var start = process.hrtime();
@@ -29,10 +59,9 @@ module.exports = {
 
 		    return function() {
 		        var end = process.hrtime(start);
+		        var ms = (end[0]*1000) + (end[1]/1000000);
 
-		        console.log(req.originalUrl);
-		        console.log(req.user.username);
-		        console.log(((end[0]*1000) + (end[1]/1000000)) + "ms");
+		        sendData('clockReport', {route: req.originalUrl, time: ms, user: req.user.username});
 
 		        var result = cached.apply(this, arguments);
 
@@ -40,7 +69,7 @@ module.exports = {
 		    };
 		})();
 	},
-	initialize: function (app, appName, staticDir) {
+	initialize: function (app, client, key, appName, staticDir) {
 		if (app == undefined) {
 			throw "Application must be defined, not initializing."
 		}
@@ -52,6 +81,8 @@ module.exports = {
 		_app = app;
 		_appName = appName;
 		_staticDir = staticDir;
+		_client = client;
+		_key = key;
 
 		app._router.stack.forEach(function(r){
 		  if (r.route && r.route.path){
@@ -59,13 +90,19 @@ module.exports = {
 		  }
 		});
 
+		socket = io.connect('localhost', {
+		    port: 3000
+		});
+
+		sendLoadAvg();
+
 		return function(req, res, next) {
-			if (req.user != undefined) {
-
+			if (req.user == undefined) {
+				sendData('pathLoaded', {route: req.originalUrl});
 			} else {
-
+				sendData('pathLoaded', {route: req.originalUrl, user: req.user.username});
 			}
-
+			
 			var isRoute = false;
 			for (var i = _routes.length - 1; i >= 0; i--) {
 				if (_routes[i] == req.originalUrl) {
@@ -75,7 +112,7 @@ module.exports = {
 			}
 
 			if (!isRoute) {
-				console.log(fs.statSync(__dirname + _staticDir + req.originalUrl)["size"]);
+				sendData('bandwidthUsed', {bytes: fs.statSync(__dirname + _staticDir + req.originalUrl)["size"]});
 			}
 			
 			next();
--- server.js
+++ server.js
@@ -7,22 +7,24 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var app = express(); 
+var io = require('socket.io')(http);
+var http = require('http').Server(app);
 
 require('./auth')();
-
 
 mongoose.connect("mongodb://localhost/statly");
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
-  extended: true
+ 	extended: true
 }));
 
 app.post('/login', Passport.authenticate('local', { successRedirect: '/login/success', failureRedirect: '/login/failure', failureFlash: false }));
 
 Passport.use(new PassportLocal(function(username,password,done){
 
-}))
+}));
+
 app.use(Passport.initialize());
 app.use(Passport.session());
 
@@ -32,4 +34,10 @@ app.use(session({
 	resave: true
 }));
 
-app.listen(3000);
+http.listen(3000, function() {
+ 	console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket) {
+ 	console.log('a client connected');
+});
